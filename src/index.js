362a7533efc58c1a9e06b7d43ff10a648db09a58
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2022-09-30 23:04:59 +0000

    Adding Swagger Documentation

--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
 require('dotenv').config();
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument1 = require('./data/swagger1.json');
+const swaggerDocument2 = require('./data/swagger2.json');
 const PORT = process.env.PORT || 9000;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/api-docs/v1', swaggerUI.serve, swaggerUI.setup(swaggerDocument1, { explorer: true }));
+app.use('/api-docs/v2', swaggerUI.serve, swaggerUI.setup(swaggerDocument2, { explorer: true }));
+
 const contacts = [
     { id: 1, name: 'Luis', phone: '+56900000000'}
 ]
 
 app.get('/api/contacts', (req, res) => {
-    return res.json({ status: 'success', message: 'List of contacts successfully', data: contacts });
+    return res.json({ status: 'success', message: 'List of contacts successfully', results: contacts });
 })
 
 app.post('/api/contacts', (req, res) => {
     contacts.push(req.body);
-    return res.json({ status: 'success', message: 'Contact created successfully', data: contacts });
+    return res.json({ status: 'success', message: 'Contact created successfully', results: contacts });
 })
 
 app.put('/api/contacts/:position', (req, res) => {
     const { position } = req.params;
     contacts[position] = req.body;
-    return res.json({ status: 'success', message: 'Contact updated successfully', data: contacts });
+    return res.json({ status: 'success', message: 'Contact updated successfully', results: contacts });
 })
 
 app.delete('/api/contacts/:position', (req, res) => {
     const { position } = req.params;
     contacts = contacts.filter((contact, index) =>  index !== position )
-    return res.json({ status: 'success', message: 'Contact deleted successfully', data: contacts });
+    return res.json({ status: 'success', message: 'Contact deleted successfully', results: contacts });
 })
 
 
